3c45a17955d1815600b617f6d20bf6961978e14a
slayer <meerutuniversity996@gmail.com>
2025-07-18 17:00:06 +0530

    Update start.py

--- AviaxMusic/plugins/bot/start.py
+++ AviaxMusic/plugins/bot/start.py
@@ -34,17 +34,17 @@ async def start_pm(client, message: Message, _):
     
     # Repeat the cycle of emojis three times
     for _ in range(2):
-        await asyncio.sleep(0.6)
-        await msg.edit("🕊️")
-        await asyncio.sleep(0.7)
-        await msg.edit("✨")
+        await asyncio.sleep(0.3)
+        await msg.edit("🔮")
+        await asyncio.sleep(0.4)
+        await msg.edit("🧋")
     
     # Show the "Starting Bot" message
-    await asyncio.sleep(0.6)
+    await asyncio.sleep(0.2)
     await msg.edit("sᴛᴀʀᴛɪɴɢ Yuki Music, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ᴀ sᴇᴄ...")
     
     # Wait for 3 seconds before deleting the "Starting Bot" message
-    await asyncio.sleep(1.75)
+    await asyncio.sleep(0.75)
     await msg.delete()
     
     if len(message.text.split()) > 1:
