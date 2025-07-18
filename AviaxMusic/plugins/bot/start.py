0afd7559e2e7fd0174a82dcd4010022ab55c235a
slayer <meerutuniversity996@gmail.com>
2025-07-18 17:08:35 +0530

    Update start.py

--- AviaxMusic/plugins/bot/start.py
+++ AviaxMusic/plugins/bot/start.py
@@ -85,7 +85,7 @@ async def start_pm(client, message: Message, _):
                 [
                     [
                         InlineKeyboardButton(text=_["S_B_8"], url=link),
-                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
+                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_GROUP),
                     ],
                 ]
             )
