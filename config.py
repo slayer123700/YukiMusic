004b0ca0b0e3ebbf1152ef4f1194c788604df61e
slayer <meerutuniversity996@gmail.com>
2025-07-19 18:17:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002745123292))
 OWNER_ID = int(getenv("OWNER_ID", "6018803920"))
 
 #YOUTUBE API's 
-API_URL = getenv("API_URL", 'https://tgmusic.fallenapi.fun') #youtube song url
-API_KEY = getenv("API_KEY", 'ca20a1_CpZ6wHK-pM2qAtdYOJHO5eSbHOg-3ftW')
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", '30DxNexGenBotsb850f6')
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
