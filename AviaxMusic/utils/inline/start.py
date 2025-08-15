a9e062e0b0aeb3081e208b6b01edac9424a64ecd
slayer <meerutuniversity996@gmail.com>
2025-08-15 18:14:05 +0530

    Update start.py

--- AviaxMusic/utils/inline/start.py
+++ AviaxMusic/utils/inline/start.py
@@ -1,25 +1,21 @@
-# AviaxMusic/utils/inline/start.py
 
 from pyrogram.types import InlineKeyboardButton
 import config
 from AviaxMusic import app
 
-# Buttons for when the bot is in a group
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons
 
-
-# Buttons for private chat with the bot
 def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇs", url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_CHAT),
         ],
         [
             InlineKeyboardButton(
