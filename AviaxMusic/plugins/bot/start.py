92b6abd9bb8de310c00aa1145cab525523f856d5
slayer <meerutuniversity996@gmail.com>
2025-08-15 18:08:00 +0530

    Update start.py

--- AviaxMusic/plugins/bot/start.py
+++ AviaxMusic/plugins/bot/start.py
@@ -3,7 +3,6 @@ import time
 from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from youtubesearchpython.__future__ import VideosSearch
 
 import config
 from AviaxMusic import app
@@ -15,113 +14,105 @@ from AviaxMusic.utils.database import (
     blacklisted_chats,
     get_lang,
     is_banned_user,
-    is_on_off,
 )
 from AviaxMusic.utils.decorators.language import LanguageStart
-from AviaxMusic.utils.formatters import get_readable_time
-from AviaxMusic.utils.inline import help_pannel, private_panel, start_panel
-from config import BANNED_USERS, OWNER_ID
+from AviaxMusic.utils.inline import help_pannel, start_panel
+from config import BANNED_USERS
 from strings import get_string
 
 
+WELCOME_VIDEO = "https://files.catbox.moe/ck28qb.mp4"
+
+# Private /start
 @app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
 @LanguageStart
 async def start_pm(client, message: Message, _):
     await add_served_user(message.from_user.id)
 
     # Loading animation
-    loading_1 = await message.reply_text("⚡")
+    loading_msg = await message.reply_text("⚡")
     for dots in ["ʟᴏᴀᴅɪɴɢ", "ʟᴏᴀᴅɪɴɢ.", "ʟᴏᴀᴅɪɴɢ..", "ᴀʟᴍᴏsᴛ ʜᴇʀᴇ..."]:
         await asyncio.sleep(0.1)
-        await loading_1.edit_text(f"<b>{dots}</b>")
-    await loading_1.delete()
+        await loading_msg.edit_text(f"<b>{dots}</b>")
+    await loading_msg.delete()
 
-    # Reaction and message effect
+    # Reaction
     await message.react("🍓", big=True)
 
-    # Temporary start animation
-    started_msg = await message.reply_text(
-        text="<b>sᴛᴀʀᴛᴇᴅ...<a href='https://files.catbox.moe/ck28qb.mp4' target='_blank'>ㅤ ㅤㅤㅤ</a></b>",
-        invert_media=True,
-        message_effect_id=5159385139981059251
-    )
-    await asyncio.sleep(0.4)
-    await started_msg.delete()
-
-    # Welcome message
+    # Welcome text
     welcome_text = (
-        f"❤️‍🩹 𝐖ᴇʟᴄᴏᴍᴇ {message.from_user.mention} (ID: <code>{message.from_user.id}</code>) 𝐓ᴏ\n\n"
+        f"❤️‍🩹 𝐖ᴇʟᴄᴏᴍᴇ {message.from_user.mention} "
+        f"(ID: <code>{message.from_user.id}</code>) 𝐓ᴏ\n\n"
         "🌙 ᴛʜᴇ ᴜʟᴛɪᴍᴀᴛᴇ ᴍᴜsɪᴄ ᴇxᴘᴇʀɪᴇɴᴄᴇ 🍇\n\n"
         "꩟ 𝐒ᴛᴜᴅɪᴏ 𝐌ᴀsᴛᴇʀ 𝐀ᴜᴅɪᴏ 𝐐ᴜᴀʟɪᴛʏ 🍇\n"
         "꩟ 𝐙ᴇʀᴏ-𝐋ᴀᴛᴇɴᴄʏ 𝐒ᴛʀᴇᴀᴍɪɴɢ 🍇\n"
         "꩟ 𝟐𝟒/𝟕 𝐀ᴄᴛɪᴠᴇ & 𝐑ᴇsᴘᴏɴsɪᴠᴇ 🍇\n"
         "꩟ 𝐒ᴍᴀʀᴛ 𝐀𝐈-𝐏ᴏᴡᴇʀᴇᴅ 𝐏ʟᴀʏʟɪsᴛs 🍇\n"
-        "꩟ 𝐋ɪɡʜᴛɴɪɴɢ-𝐅ᴀsᴛ 𝐒ᴇᴀʀᴄʜᴇs 🍇\n"
-        "꩟ 𝐘ᴏᴜʀ 𝐏ʀᴏғɪʟᴇ 🍇\n\n"
-        "꩟ 𝐍ᴀᴍᴇ :- \n"
-        "꩟ 𝐈'𝐃 :-\n\n"
+        "꩟ 𝐋ɪɡʜᴛɴɪɴɢ-𝐅ᴀsᴛ 𝐒ᴇᴀʀᴄʜᴇs 🍇\n\n"
+        f"꩟ 𝐘ᴏᴜʀ 𝐏ʀᴏғɪʟᴇ 🍇\n"
+        f"꩟ 𝐍ᴀᴍᴇ :- {message.from_user.first_name}\n"
+        f"꩟ 𝐈'𝐃 :- {message.from_user.id}\n\n"
         "🎧 𝐑ᴇᴀᴅʏ 𝐓ᴏ 𝐄xᴘᴇʀɪᴇɴᴄᴇ 𝐌ᴜsɪᴄ 𝐋ɪᴋᴇ 𝐍ᴇᴠᴇʀ 𝐁ᴇғᴏʀᴇ? 💃🏼\n\n"
         "𝐉ᴏɪɴ 𝐎ᴜʀ 𝐌ᴜsɪᴄ 𝐑ᴇᴠᴏʟᴜᴛɪᴏɴ 𝐓ᴏᴅᴀʏ! 🎸"
     )
 
-    # FIXED: help_pannel already returns InlineKeyboardMarkup
-    await message.reply_text(
-        text=welcome_text,
-        reply_markup=help_pannel(_),
-        invert_media=True,
-        message_effect_id=5159385139981059251
+    # Send video + welcome text + buttons in one message
+    await message.reply_video(
+        video=WELCOME_VIDEO,
+        caption=welcome_text,
+        reply_markup=help_pannel(_)
     )
 
 
+# Group /start
 @app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
 @LanguageStart
 async def start_gp(client, message: Message, _):
-    out = start_panel(_)  # returns a list
-    uptime = int(time.time() - _boot_)
-
     await message.react("🍓", big=True)
+    await add_served_chat(message.chat.id)
 
     welcome_text = (
-        f"❤️‍🩹 𝐖ᴇʟᴄᴏᴍᴇ {message.from_user.mention} (ID: <code>{message.from_user.id}</code>) 𝐓ᴏ\n\n"
+        f"❤️‍🩹 𝐖ᴇʟᴄᴏᴍᴇ {message.from_user.mention} "
+        f"(ID: <code>{message.from_user.id}</code>) 𝐓ᴏ\n\n"
         "🌙 ᴛʜᴇ ᴜʟᴛɪᴍᴀᴛᴇ ᴍᴜsɪᴄ ᴇxᴘᴇʀɪᴇɴᴄᴇ 🍇\n\n"
         "꩟ 𝐒ᴛᴜᴅɪᴏ 𝐌ᴀsᴛᴇʀ 𝐀ᴜᴅɪᴏ 𝐐ᴜᴀʟɪᴛʏ 🍇\n"
         "꩟ 𝐙ᴇʀᴏ-𝐋ᴀᴛᴇɴᴄʏ 𝐒ᴛʀᴇᴀᴍɪɴɢ 🍇\n"
         "꩟ 𝟐𝟒/𝟕 𝐀ᴄᴛɪᴠᴇ & 𝐑ᴇsᴘᴏɴsɪᴠᴇ 🍇\n"
         "꩟ 𝐒ᴍᴀʀᴛ 𝐀𝐈-𝐏ᴏᴡᴇʀᴇᴅ 𝐏ʟᴀʏʟɪsᴛs 🍇\n"
-        "꩟ 𝐋ɪɡʜᴛɴɪɴɢ-𝐅ᴀsᴛ 𝐒ᴇᴀʀᴄʜᴇs 🍇\n"
-        "꩟ 𝐘ᴏᴜʀ 𝐏ʀᴏғɪʟᴇ 🍇\n\n"
-        "꩟ 𝐍ᴀᴍᴇ :- \n"
-        "꩟ 𝐈'𝐃 :-\n\n"
+        "꩟ 𝐋ɪɡʜᴛɴɪɴɢ-𝐅ᴀsᴛ 𝐒ᴇᴀʀᴄʜᴇs 🍇\n\n"
+        f"꩟ 𝐘ᴏᴜʀ 𝐏ʀᴏғɪʟᴇ 🍇\n"
+        f"꩟ 𝐍ᴀᴍᴇ :- {message.from_user.first_name}\n"
+        f"꩟ 𝐈'𝐃 :- {message.from_user.id}\n\n"
         "🎧 𝐑ᴇᴀᴅʏ 𝐓ᴏ 𝐄xᴘᴇʀɪᴇɴᴄᴇ 𝐌ᴜsɪᴄ 𝐋ɪᴋᴇ 𝐍ᴇᴠᴇʀ 𝐁ᴇғᴏʀᴇ? 💃🏼\n\n"
         "𝐉ᴏɪɴ 𝐎ᴜʀ 𝐌ᴜsɪᴄ 𝐑ᴇᴠᴏʟᴜᴛɪᴏɴ 𝐓ᴏᴅᴀʏ! 🎸"
     )
 
-    # FIXED: start_panel returns a list, so wrap in InlineKeyboardMarkup
-    await message.reply_text(
-        text=welcome_text,
-        reply_markup=InlineKeyboardMarkup(out),
-        invert_media=True,
-        message_effect_id=5159385139981059251
+    await message.reply_video(
+        video=WELCOME_VIDEO,
+        caption=welcome_text,
+        reply_markup=InlineKeyboardMarkup(start_panel(_))
     )
 
-    await add_served_chat(message.chat.id)
 
-
+# Auto-welcome when bot joins a group
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):
     for member in message.new_chat_members:
         try:
             language = await get_lang(message.chat.id)
             _ = get_string(language)
+
             if await is_banned_user(member.id):
                 try:
                     await message.chat.ban_member(member.id)
                 except:
                     pass
+
             if member.id == app.id:
                 if message.chat.type != ChatType.SUPERGROUP:
                     await message.reply_text(_["start_4"])
                     return await app.leave_chat(message.chat.id)
+
                 if message.chat.id in await blacklisted_chats():
                     await message.reply_text(
                         _["start_5"].format(
@@ -133,17 +124,18 @@ async def welcome(client, message: Message):
                     )
                     return await app.leave_chat(message.chat.id)
 
-                out = start_panel(_)
-                await message.reply_text(
-                    _["start_3"].format(
+                await message.reply_video(
+                    video=WELCOME_VIDEO,
+                    caption=_["start_3"].format(
                         message.from_user.first_name,
                         app.mention,
                         message.chat.title,
                         app.mention,
                     ),
-                    reply_markup=InlineKeyboardMarkup(out)
+                    reply_markup=InlineKeyboardMarkup(start_panel(_))
                 )
                 await add_served_chat(message.chat.id)
                 await message.stop_propagation()
+
         except Exception as ex:
             print(ex)
